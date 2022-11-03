dfb7c663bfa8623db55f881272b642aa77b935e5
Shota TAMURA <r3108.sh@gmail.com>
2022-11-03 11:52:52 +0900

    Add square x / o

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,44 @@
+import { useMemo, useState } from 'react';
 import './App.css'
 
-const Square = (props: { value: number }) => {
+const Square = (props: { value: number, onClick: () => void }) => {
   return (
-    <button className="square">
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
 }
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(false);
+
+  const status = useMemo(() => 'Next player: ' + (xIsNext ? 'X' : 'O'), [xIsNext]);
+
+  const handleClick = (i: number) => {
+    const newSquares = squares.slice();
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
