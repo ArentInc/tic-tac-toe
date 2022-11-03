46b14c6b77fb45d435254f91f48047c34a1940dd
Shota TAMURA <r3108.sh@gmail.com>
2022-11-03 11:07:58 +0900

    Add UI base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+const Square = (props: { value: number }) => {
+  return (
+    <button className="square">
+      {props.value}
+    </button>
+  );
+}
+
+const Board = () => {
+  return (
+    <div>
+      <div className="board-row">
+        <Square value={0} />
+        <Square value={1} />
+        <Square value={2} />
+      </div>
+      <div className="board-row">
+        <Square value={3} />
+        <Square value={4} />
+        <Square value={5} />
+      </div>
+      <div className="board-row">
+        <Square value={6} />
+        <Square value={7} />
+        <Square value={8} />
+      </div>
+    </div>
+  );
+}
+
+const Game = () => {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="errors"></div>
+      <div id="root">
+        <Game />
+      </div>
+    </>
   );
 }
 
